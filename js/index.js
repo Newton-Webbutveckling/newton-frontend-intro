bac5d33a5fa8a6fd843cab684283104f5b4b589c
Samson <31880780+Zerueru90@users.noreply.github.com>
2021-08-19 15:37:04 +0200

    börjar se bra ut

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
